8eb550a2a204b5f0ab9aa3dc7a08c2790a192041
KDanielRosas <k.daniel.rosas99@gmail.com>
2023-04-05 16:46:28 -0600

    Add project files.

---
+++ PL/Controllers/RandomUserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class RandomUserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ PL/wwwroot/js/RandomUser/Scripts.js
@@ -0,0 +1,30 @@
+﻿
+function GetRandomUser() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function (result) {
+            var fila =
+            "<tr>" +
+                "<td class='text-center'>" + result.results[0].name.title + ". " + result.results[0].name.first +
+                " " + result.results[0].name.last + "</td>" +
+                "<td class='text-center'>" + result.results[0].email + "</td>" + 
+                "<td class='text-center'>" + result.results[0].location.country + ", " +
+                result.results[0].location.state + ", " + result.results[0].location.city + "</td>" + 
+                "<td class='text-center'>" + result.results[0].gender + "</td>" + 
+                "<td class='text-center'><img src='" + result.results[0].picture.large + "'></img></td>" + 
+            "</tr>"
+
+            $("#tblUsers tbody").append(fila);
+        },
+        error: function (ex) {
+            alert('Error al realizar la consulta: ' + ex);
+        }
+    });
+}
+
+
+function LimpiarTabla() {
+    $('#tblUsers tbody').empty();
+}
